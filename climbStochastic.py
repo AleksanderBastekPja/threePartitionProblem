b9353e0419f191b514cef5fc241d1223a3a82f39
AleksanderBastekPJA <s27454@pjwstk.edu.pl>
2025-06-21 13:24:06 +0200

    Add climbStochastic and delete test.py

---
+++ climbStochastic.py
@@ -0,0 +1,46 @@
+import time
+from utils import loss, makeRandomSolution, generateNeighbours, showResults
+import sys
+from random import randint
+
+def hillClimbingStochastic(nums, maxIteration):
+    current = makeRandomSolution(len(nums))
+    for i in range(maxIteration):
+        neighbours = generateNeighbours(current)
+        neighbour = neighbours[randint(0,len(neighbours)-1)]
+
+        if loss(neighbour, nums) <= loss(current, nums):
+            current = neighbour
+        if loss(current, nums) == 0:
+            return current,i
+    return current, maxIteration
+
+
+def main(numbers, iterationAmount):
+    numsLen = len(numbers)
+    if (numsLen % 3 != 0):
+        raise ValueError("Ilość podanych liczb nie jest podzielna przez trzy!")
+    if (sum(numbers) % (numsLen // 3) != 0):
+        raise ValueError("Suma liczb nie podzielna przez liczbę grup!")
+
+    timeBefore = time.time()
+    result = hillClimbingStochastic(numbers, iterationAmount)
+    timeAfter = time.time() - timeBefore
+
+    showResults(numbers, result, timeAfter)
+
+if __name__ == "__main__":
+    numbers = [int(arg) for arg in sys.argv[2:]]
+    main(numbers, int(sys.argv[1]))
+
+
+# def hill_climbing_stochastic(list_to_sort, loss, max_iterations):
+#     current = random_solution(list_to_sort)
+#     for i in range(max_iterations):
+#         neighbours = generate_neighbours(current)
+#         neighbour = neighbours[randint(0,len(neighbours)-1)]
+#         if loss(neighbour) <= loss(current):
+#             current = neighbour
+#         if loss(current) == 0:
+#             return current,i
+#     return current, max_iterations
